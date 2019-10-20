21feb8151c659d754d41dca80836662bde2bb618
hengjiwang <hengjw@uw.edu>
2019-10-20 00:33:22 -0700

    Add scraper.py

--- scrape_novel/chapter_parser.py
+++ scrape_novel/chapter_parser.py
@@ -2,6 +2,25 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from collections import defaultdict
 import regex as re
+import pandas as pd
+import numpy as np
+
+def load_table(people_file):
+    '''
+    :param people_file: Path to the .csv file that stores the valid names
+    :type people_file: str
+    :return: A dictionary of valid names
+    :rtype: defaultdict
+    '''
+    names = defaultdict(bool)
+    df = pd.read_csv(people_file)
+    lnames = df['name_en'].values
+    new_names = np.array(['Emperor Xian', 'Da Qiao', 'Xiao Qiao', 
+    'Diao Chan', 'Yue Jing', 'Zhang Ba', 'Zhang Jue', 'Zhang Lian'])
+    lnames = np.concatenate((lnames, new_names))
+    for name in lnames:  names[name] = True
+
+    return names
 
 def load_page(url):
     '''
@@ -45,7 +64,21 @@ def add_edges(g, names):
             pair = (names[i], names[j])
             g[pair] += 1
 
-def parse_chapter(url):
+def drop_names(g, valid_names):
+    '''
+    :param g: Graph that stores edges
+    :type g: defaultdict
+    :param valid_names: Table that stores the valid names
+    :type valid_names: defaultdict 
+    :rtype: None
+    '''
+    keys = list(g.keys())
+    for pair in keys:
+        if pair[0] not in valid_names or pair[1] not in valid_names:
+            del g[pair]
+
+
+def parse_chapter(url, valid_names):
     '''
     :param url: URL of the target chapter
     :type url: str
@@ -61,10 +94,13 @@ def parse_chapter(url):
         if len(names) > 1:
             add_edges(graph, names)
 
+    drop_names(graph, valid_names)
+
     return graph
 
 if __name__ == "__main__":
-    parse_chapter("https://www.threekingdoms.com/001.htm")
+    valid_names = load_table("./data/people.csv")
+    parse_chapter("https://www.threekingdoms.com/001.htm", valid_names)
 
     
 
--- scrape_novel/scraper.py
+++ scrape_novel/scraper.py
@@ -2,4 +2,48 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from collections import defaultdict
 import regex as re
+from parse_chapter import parse_chapter, load_page, load_table
+import pandas as pd
+import sys
+
+def build_graph(base_url, valid_names, start_chapter=1, end_chapter=120):
+    '''
+    :param base_url: URL to start scraping
+    :type base_url: str
+    :return: Combined Graph
+    :rtype: list
+    '''
+
+    graph = defaultdict(lambda:0)
+    res = []
+
+    for j in range(start_chapter, end_chapter+1):
+        if j < 10:
+            url = base_url + '00' + str(j) + ".htm"
+        elif j < 100:
+            url = base_url + '0' + str(j) + ".htm"
+        else:
+            url = base_url + str(j) + ".htm"
+        
+        g = parse_chapter(url, valid_names)
+        for key in g.keys():
+            graph[key] += g[key]
+
+    for key in graph.keys():
+        res.append((key[0], key[1], graph[key]))
+
+    return res
+
+    
+
+
+if __name__ == "__main__":
+    valid_names = load_table("./data/people.csv")
+    start = int(sys.argv[1])
+    end = int(sys.argv[2])
+    graph = build_graph("https://www.threekingdoms.com/", valid_names, start, end)
+    header = ['p1', 'p2', 'count']
+    df_graph = pd.DataFrame(columns=header, data=graph)
+    df_graph.to_csv('./data/graph-' + str(start) + '-' + str(end) + '.csv', index = False)
+    
 
